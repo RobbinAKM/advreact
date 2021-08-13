5046482853b96593e5a811fdfb94dffeda538712
aung min <robbinakm12@hotmail.com>
2021-08-13 11:39:07 +0900

    added amplify

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
-import React,{useState,useReducer,useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Amplify from "aws-amplify";
+import awsConfig from "./aws-exports";
+import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
-const Checkbox=({children})=>{
-  const [checked,setCheck]=useState(true)
-  return React.Children.map(children,(child)=>{
-    const clone= React.cloneElement(child,{
+Amplify.configure(awsConfig);
+
+const Checkbox = ({ children }) => {
+  const [checked, setCheck] = useState(true);
+  return React.Children.map(children, (child) => {
+    const clone = React.cloneElement(child, {
       checked,
-      setCheck
-    })
-    return clone
-  })
-}
+      setCheck,
+    });
+    return clone;
+  });
+};
 
-const Label=({children,setCheck})=>{
-  return <h1 onClick={()=>{setCheck(state=>!state)}}>{children}</h1>
-}
+const Label = ({ children, setCheck }) => {
+  return (
+    <h1
+      onClick={() => {
+        setCheck((state) => !state);
+      }}
+    >
+      {children}
+    </h1>
+  );
+};
 
-const Input =({setCheck,checked})=>{
-  return <input
-  type="checkbox"
-  checked={checked}
-  onChange={(e)=>{
-    setCheck(e.target.checked)
-    }}
-  />
-}
+const Input = ({ setCheck, checked }) => {
+  return (
+    <input
+      type="checkbox"
+      checked={checked}
+      onChange={(e) => {
+        setCheck(e.target.checked);
+      }}
+    />
+  );
+};
 
 function App() {
-
   return (
-    <div className="App">
-     <Checkbox>
-      <Label>Welcome</Label>
-      <Input/>
-     </Checkbox>
-       </div>
+    <AmplifyAuthenticator>
+      <div className="App">
+        <Checkbox>
+          <Label>Welcome</Label>
+          <Input />
+        </Checkbox>
+        <AmplifySignOut />
+      </div>
+    </AmplifyAuthenticator>
   );
 }
 
